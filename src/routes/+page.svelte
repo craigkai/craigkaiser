aee8f571f92e5a6863bdc421abcf70539c376b24
Craig Kaiser <craig@kaiserengineering.io>
2023-10-17 08:30:56 -0400

    add footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,3 +62,10 @@
 		</ul>
 	</div>
 </div>
+
+<div class="flex justify-center absolute inset-x-0 bottom-0">
+	source code on <a
+		class="ml-1 font-small text-blue-600 dark:text-blue-500 hover:underline"
+		href="https://github.com/craigkai/craigkaiser">Github</a
+	>
+</div>
