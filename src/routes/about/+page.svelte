f41a21e54ba05b484c24693c3770d8fbce77259b
Craig Kaiser <craig@kaiserengineering.io>
2023-10-18 12:21:24 -0400

    update about me page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,45 @@
 <script lang="ts"></script>
 
-<div class="m-4 flex justify-content-center flex-col w-1/2">
-	<img src="me.jpg" alt="Craig" width="345" class="rounded-lg" />
-	<p>
-		Thanks for stopping by. I'm a software engineer with a passion for technology and building
-		innovate and inspiring products!
-	</p>
+<div class="flex justify-content-center flex-col sm:w-full lg:w-1/2">
+	<img src="me.jpg" alt="Craig" width="345" class="m-4 rounded-lg" />
+	<span class="description m-4">
+		<p>
+			Welcome! I'm Craig Kaiser, a dedicated software engineer driven by an unwavering passion for
+			technology and the creation of innovative, inspiring products.
+		</p>
 
-	<p>
-		I am an Electrical Engineer by schooling turned software engineer after seeing how much more fun
-		coding is compared to circuits!
-	</p>
+		<p>
+			My journey into the world of software engineering took an unconventional path. Originally
+			schooled as an Electrical Engineer, I couldn't resist the allure of coding and its potential
+			for creativity and impact. The transition was a revelation, and I haven't looked back since.
+		</p>
+
+		<p>
+			As a software engineer, I relish the opportunity to bring ideas to life, harnessing the power
+			of technology to solve real-world problems and shape the digital landscape. I find immense joy
+			in the art of coding and the thrill of crafting software that not only functions flawlessly
+			but also inspires and transforms.
+		</p>
+
+		<p>
+			My diverse background in electrical engineering provides me with a unique perspective,
+			enabling me to approach software development with a holistic understanding of both hardware
+			and software ecosystems.
+		</p>
+
+		<p>
+			When I'm not immersed in lines of code, you can often find me exploring the latest tech
+			trends, tinkering with side projects, or delving into the world of AI and machine learning.
+			I'm a firm believer that technology has the potential to reshape our future and improve lives
+			in profound ways.
+		</p>
+
+		<p>
+			If you share my passion for technology and the pursuit of groundbreaking solutions, I'd love
+			to connect and collaborate. Let's embark on a journey of innovation and bring remarkable ideas
+			to fruition.
+		</p>
+
+		<p>Feel free to explore my portfolio and reach out to me!.</p>
+	</span>
 </div>
