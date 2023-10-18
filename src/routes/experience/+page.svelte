fc4f6c7f3dfa9ff77a1f043f103121939c960795
Craig Kaiser <craig@kaiserengineering.io>
2023-10-18 11:30:26 -0400

    add scroll effect to experiences page

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,31 +1,131 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+
 	/** @type {import('./$types').PageData} */
 	export let data: any;
+
+	// let currentTime = duration * scrollY / total scroll height
+	let scrollY: number;
+	let time: number;
+	let duration: number;
 
 	const projects = data?.projects.sort((a, b) => {
 		return a.id - b.id;
 	});
+	$: {
+		const totalScroll = document.documentElement.scrollHeight - window.innerHeight;
+		time = duration * (scrollY / totalScroll);
+	}
 </script>
 
-<div class="flex flex-col lg:p-16 md:m-4 sm:m-4">
-	<div class="lg:text-5xl self-start font-bold leading-tight text-3xl">Experience:</div>
+<svelte:window bind:scrollY />
 
-	<div class="grid grid-rows-1 gap-4 m-16 w-1/2">
-		{#each projects as project}
-			<div class="flex place-items-center">
-				{#each project?.tech || [] as technology}
-					<div class="pr-2">
-						<Icon class="stroke-blue-400" icon={technology?.icon} />
-					</div>
-				{/each}
-				<a class="text-xl place-items-center" href={project.link}>{project.name}:</a>
-			</div>
-			<span class="text-sm ml-6">{project.description}</span>
-
-			{#if project?.media}
-				<img class="" src={project?.media} alt="alt" />
-			{/if}
-		{/each}
-	</div>
+<div class="video-container">
+	<video
+		bind:currentTime={time}
+		bind:duration
+		preload="metadata"
+		muted
+		src="https://int.nyt.com/newsgraphics/2020/beirut-explosion-video/main/warehouse-800.mp4"
+	/>
 </div>
+
+<div id="scroll-container">
+	<div class="heading">
+		<h1>My Experience!</h1>
+	</div>
+
+	{#each projects as project, i}
+		<div class="str str{i}">
+			<p class="rounded-lg">
+				<span class="flex">
+					{#each project?.tech || [] as technology}
+						<div class="pr-2">
+							<Icon class="stroke-blue-400" icon={technology?.icon} />
+						</div>
+					{/each}
+				</span>
+				<a class="text-xl place-items-center" href={project.link}>{project.name}:</a>
+
+				<span class="text-sm ml-6">{project.description}</span>
+
+				{#if project?.media}
+					<img class="" src={project?.media} alt="alt" />
+				{/if}
+			</p>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.video-container {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		overflow: hidden;
+	}
+	.video-container video {
+		min-width: 100%;
+		min-height: 100%;
+		width: auto;
+		height: auto;
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	#scroll-container {
+		height: 5000px;
+	}
+
+	.heading {
+		position: absolute;
+		top: 200px;
+		width: 100%;
+		text-align: center;
+		color: white;
+	}
+
+	.heading h1 {
+		color: white;
+		font-size: 4em;
+	}
+	.str {
+		position: absolute;
+		display: flex;
+		flex-wrap: wrap;
+		left: 50%;
+		transform: translate(-50%, 0);
+		line-height: 1.5;
+		opacity: 0.8;
+	}
+
+	.str p {
+		color: white;
+		background-color: black;
+		padding: 2em;
+		min-width: 20em;
+	}
+
+	.str p:nth-child(2) {
+		background-color: white;
+		color: black;
+	}
+
+	.str0 {
+		top: 1000px;
+	}
+
+	.str1 {
+		top: 2000px;
+	}
+
+	.str2 {
+		top: 3000px;
+	}
+
+	.str3 {
+		top: 4000px;
+	}
+</style>
