6b98c2d98dca476e72951b7ebc1febb7a4891645
Craig Kaiser <craig@kaiserengineering.io>
2023-10-23 12:29:02 -0400

    add tooltips for experience tech icons

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -17,7 +17,17 @@
 				<div class="flex place-items-center">
 					{#each project?.tech || [] as technology}
 						<div class="pr-2">
-							<Icon class="stroke-blue-400" icon={technology?.icon} />
+							<div class="group inline-block cursor-help">
+								<div class="absolute">
+									<span
+										class="absolute flex flex-col hidden group-hover:flex -left-10 -top-2 -translate-y-full w-24 px-2 py-1 bg-blue-500 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700"
+									>
+										<div class="text-xs">{technology?.label}</div>
+									</span>
+								</div>
+
+								<Icon class="stroke-blue-400" icon={technology?.icon} />
+							</div>
 						</div>
 					{/each}
 					<span class="text-xl place-items-center">{@html project.name}:</span>
@@ -30,6 +40,7 @@
 			</div>
 		{/each}
 	</div>
+	<div>here</div>
 </div>
 
 <style>
