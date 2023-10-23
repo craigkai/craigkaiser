f0697043a6597cd46db3f3fc83cd271870ec03a4
Craig Kaiser <craig@kaiserengineering.io>
2023-10-23 12:48:50 -0400

    specify unique IPS

--- src/components/Visitors.svelte
+++ src/components/Visitors.svelte
@@ -4,6 +4,6 @@
 
 <div class="bottom-0 -z-5 fixed left-4 p-4">
 	<div class="z-0 place-content-center bottom-0 lg:w-full md:w-full w-1/2">
-		Visitors (unique) count #{views}
+		Visitors (unique IPs) count #{views}
 	</div>
 </div>
